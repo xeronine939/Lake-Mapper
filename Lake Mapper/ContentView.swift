ecd85c725c2e5fceef8f55850d85cc11e8626f79
Jon Miron <jon@mirons.ca>
2025-07-12 17:17:24 -0400

    Initial Commit

---
+++ Lake Mapper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lake Mapper
+//
+//  Created by Jonathan Miron on 2025-07-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lake Mapper/Lake_MapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lake_MapperApp.swift
+//  Lake Mapper
+//
+//  Created by Jonathan Miron on 2025-07-12.
+//
+
+import SwiftUI
+
+@main
+struct Lake_MapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
